b881231383d2a89423fff01df350765651cbb7fe
ioandanielc <ioandaniel.craciun@gmail.com>
2025-10-20 21:03:59 +0200

    added command to mount the LRZ folders from the $NEWROOT - correction

--- src/sph2img/utils/mount_LRZ.sh
+++ src/sph2img/utils/mount_LRZ.sh
@@ -1,1 +1,1 @@
-sshfs -o volname=LRZ,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,auto_cache,defer_permissions,IdentityFile=~/.ssh/id_rsa \\n  ge57gon2@login.ai.lrz.de:/dss/dssfs04/lwp-dss-0002/pn36ni/pn36ni-dss-0000/ge57gon2/projects \\n  ~/LRZ\n
+sshfs -o volname=LRZ,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,auto_cache,defer_permissions,IdentityFile=~/.ssh/id_rsa \\n  ge57gon2@login.ai.lrz.de:/dss/dssfs04/lwp-dss-0002/pn36ni/pn36ni-dss-0000/ge57gon2/projects
